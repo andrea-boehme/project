60b388c8ad1e6fca48516fefd422ae0831e8cd85
andrea-boehme <99185432+andrea-boehme@users.noreply.github.com>
2023-03-29 14:07:59 +0200

    role and permission

--- src/main/java/de/allianz/project/service/UserDetailsService.java
+++ src/main/java/de/allianz/project/service/UserDetailsService.java
@@ -1,6 +1,7 @@
 package de.allianz.project.service;
 
 import de.allianz.project.config.PasswordEncoderConfig;
+import de.allianz.project.entity.Role;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,6 +9,7 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 
 @RequiredArgsConstructor
 @Configuration // damit Klasse von Spring gefunden wird
@@ -16,20 +18,30 @@ public class UserDetailsService {
     private final PasswordEncoder passwordEncoder;
 
     @Bean
-    public InMemoryUserDetailsManager users() {
+    public UserDetailsManager users() {
 
         UserDetails user = User.builder()
                 .username("user")
                 //.password(passwordEncoder.encode("userPassword"))
                 .password("{bcrypt}$2a$10$GRLdNijSQMUvl/au9ofL.eDwmoohzzS7.rmNSJZ.0FxO/BTk76klW") // steht für "password", muss man encoden
-                .roles("USER")
+                //.roles("USER")
+                .authorities(Role.MEMBER.getGrantedAuthorities())
                 .build();
+
         UserDetails admin = User.builder()
                 .username("admin")
                 //.password(passwordEncoder.encode("adminPassword"))
                 .password("{noop}admin-password")
-                .roles("USER", "ADMIN")
+                //.roles("USER", "ADMIN")
+                .authorities(Role.ADMIN.getGrantedAuthorities())
                 .build();
-        return new InMemoryUserDetailsManager(user, admin);
+
+        UserDetails analyst = User.builder()
+                .username("analyst")
+                .password("{noop}analyst-password")
+                .authorities(Role.ANALYST.getGrantedAuthorities())
+                .build();
+
+        return new InMemoryUserDetailsManager(user, admin, analyst);
     }
 }
